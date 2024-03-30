bfbdea94a9fcded6c170b08602febc74405fdb13
liziwen <liziwen@richinfo.cn>
2024-03-31 07:26:55 +0800

    30 个人中心页动画效果实现

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,18 +1,59 @@
 // pages/personal/personal.js
+
+let startY = 0; // 手指起始的坐标
+let moveY = 0;  // 手指移动的坐标
+let moveDistance = 0; // 手指移动的距离
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    coverTransform: 'translateY(0)',
+    coveTransition: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    
+  },
 
+  handleTouchStart(event) {
+    console.log('start')
+    startY = event.touches[0].clientY;
+
+    this.setData({
+      coverTransition: ''
+    })
+  },
+
+  handleTouchMove(event) {
+    moveY = event.touches[0].clientY;
+    moveDistance = moveY - startY;
+    console.log(moveDistance);
+
+    if(moveDistance <= 0 ){
+      return;
+    }
+    if(moveDistance >= 80){
+      moveDistance = 80;
+    }
+    // 动态更新 coverTransform 的状态值
+    this.setData({
+      coverTransform: `translateY(${moveDistance}rpx)`,
+    })
+  },
+
+  handleTouchEnd() {
+    console.log('end')
+    // 动态更新 coverTransform 的状态值
+    this.setData({
+      coverTransform: `translateY(0rpx)`,
+      coverTransition: 'transform 1s linear'
+    })
   },
 
   /**
