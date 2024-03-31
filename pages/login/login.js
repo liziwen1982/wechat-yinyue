4236fba54b5a803908ba03fce6c3f321b3e77e05
liziwen <liziwen@richinfo.cn>
2024-03-31 19:31:24 +0800

    33 前端验证实现

--- pages/login/login.js
+++ pages/login/login.js
@@ -42,6 +42,50 @@ Page({
     })
   },
 
+  // 登陆的回调
+  login(){
+    // 1.收集数据
+    let {phone, password} = this.data;
+    console.log(phone, password)
+    // 2.前端验证
+    /*
+    * 手机号验证：
+    *   1. 内容为空
+    *   2. 手机号格式不正确
+    *   3. 手机号格式正确，验证通过
+    * */
+
+    if(!phone){
+      wx.showToast({
+        title: '手机号不能为空',
+        icon: 'none'
+      })
+      return;
+    }
+
+    // 定义正则表达式
+    let phoneReg = /^1(3|4|5|6|7|8|9)\d{9}$/;
+    if(!phoneReg.test(phone)){
+      wx.showToast({
+        title:'手机号格式错误',
+        icon: 'none'
+      })
+      return;
+    }
+
+    if(!password){
+      wx.showToast({
+        title: '密码不能为空',
+        icon: 'none'
+      })
+      return;
+    }
+
+    wx.showToast({
+      title: '前端验证成功'
+    })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
